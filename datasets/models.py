85f2a4305c1604531d4b1b51b7f670d88031afa1
Karen Guzman <krngzmn10@gmail.com>
2025-03-26 22:50:56 -0400

    added test file

---
+++ datasets/models.py
@@ -0,0 +1,8 @@
+from mongoengine import Document, StringField, DateTimeField
+
+# A model of the metadata for each dataset
+class Dataset(Document):
+    name = StringField(required=True)
+    description = StringField()
+    file_url = StringField(required=True)  # Link to the actual file stored in S3
+    uploaded_at = DateTimeField()
